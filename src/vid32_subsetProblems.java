52e8213eafa82e60855a0f14cec36c1706939aa2
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-04-29 19:41:20 +0530

    Added Subset Problem

---
+++ src/vid32_subsetProblems.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class vid32_subsetProblems {
+    public static void main(String[] args) {
+        subset("", "abc");
+    }
+
+    static void subset(String p, String up){
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+
+        subset(p, up.substring(1));
+        subset(p+ch, up.substring(1));
+    }
+}
