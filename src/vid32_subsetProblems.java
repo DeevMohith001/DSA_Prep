2f936c61311750ac0d5beb1609dea015c17beda3
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-06-21 04:26:38 +0530

    Updated subset problems

--- src/vid32_subsetProblems.java
+++ src/vid32_subsetProblems.java
@@ -2,7 +2,8 @@ import java.util.ArrayList;
 
 public class vid32_subsetProblems {
     public static void main(String[] args) {
-        subset("", "abc");
+//        subset("", "abc");
+        System.out.println(subseqReturn("", "abc"));
     }
 
     static void subset(String p, String up){
@@ -15,5 +16,20 @@ public class vid32_subsetProblems {
 
         subset(p, up.substring(1));
         subset(p+ch, up.substring(1));
+    }
+
+    static  ArrayList<String> subseqReturn(String p, String up){
+        if (up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String> left = subseqReturn(p+ch, up.substring(1));
+        ArrayList<String> right = subseqReturn(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
     }
 }
