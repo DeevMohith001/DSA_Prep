48f7c5c63d78bf1763155045bc3038cc55f2d9e7
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-06-21 05:41:32 +0530

    Added more content in subset problems

---
+++ src/vid32_Ascii.java
@@ -0,0 +1,8 @@
+public class vid32_Ascii {
+    public static void main(String[] args) {
+        char ch = 'a';
+//        System.out.println(ch+0); // to print ascii value
+        System.out.println((char)(ch+1));
+    }
+
+}
