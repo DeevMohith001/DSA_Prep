9e489d090c973b56b3470920f768d93b86db2fa3
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-08-08 04:07:35 +0530

    worked on static

---
+++ src/OOPs/Packages/staticExample/Main.java
@@ -0,0 +1,7 @@
+package OOPs.Packages.staticExample;
+
+public class Main {
+    public static void main(String[] args) {
+        Human me = new Human(25, "mohit", 2000, true); // No need to import if files are in same folder/package.
+    }
+}
