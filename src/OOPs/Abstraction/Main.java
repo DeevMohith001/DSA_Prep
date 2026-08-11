3c8b9eac4d0d9c798f701922a0e45310cba34f24
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-08-12 04:48:51 +0530

    worked on Abstraction and encapsulation

---
+++ src/OOPs/Abstraction/Main.java
@@ -0,0 +1,15 @@
+package OOPs.Abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+// Abstraction --> Hiding the unnecessary details but only showing essential information.
+// Abstraction vs Encapsulation -->
+//      Abstraction is solving a design level issue. Encapsulation is solving an implementation issue.
+
+// Encapsulation is hiding something internally, Abstraction will provide some of the outside methods we can use.
+// Abstraction focuses on the external stuff, and encapsulation focuses on the internal working.
+// Abstraction is the process of gaining information, Encapsulation is a process of containing the information.
---
+++ src/OOPs/Encapsulation/Main.java
@@ -0,0 +1,21 @@
+package OOPs.Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+// Encapsulation --> Wrapping up the implementation of the data members and the methods inside a class.
+// It basically hides the code or data into a single entity or a unit so that can be protected from the outside world.
+
+// Abstraction vs Encapsulation -->
+//      Abstraction is solving a design level issue. Encapsulation is solving an implementation issue.
+
+// Encapsulation is hiding something internally, Abstraction will provide some of the outside methods we can use.
+// Abstraction focuses on the external stuff, and encapsulation focuses on the internal working.
+// Abstraction is the process of gaining information, Encapsulation is a process of containing the information.
+
+// Data hiding and Encapsulation:
+//      Data hiding focuses on data security. e.g. private keyword.
+//      Encapsulation focuses on hiding the complexity of the system. here data can be public or private.
+//      Encapsulation is a process in data hiding.
