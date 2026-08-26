db443f43c6b7017942aec7f9d9dbdd7a8025d1bf
DeevMohith001 <mohitrajdeev2006@gmail.com>
2026-08-26 15:49:19 +0530

    String recursion, phonePad and dice problems

---
+++ src/Recursion/vid34_01_phonePad_problem.java
@@ -0,0 +1,51 @@
+package Recursion;
+
+import java.util.ArrayList;
+
+public class vid34_01_phonePad_problem {
+    public static void main(String[] args) {
+        phone("", "12");
+    }
+    static void phone(String p, String up){
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+
+        for (int i = (digit-1)*3; i < digit*3; i++) {
+            char ch = (char) ('a' + i);
+            phone(p+ch, up.substring(1));
+        }
+    }
+
+    static ArrayList<String> padRet(String p, String up) {
+        if (up.isEmpty()) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            list.addAll(padRet(p + ch, up.substring(1)));
+        }
+        return list;
+    }
+
+    static int padCount(String p, String up) {
+        if (up.isEmpty()) {
+            return 1;
+        }
+        int count = 0;
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+        for (int i = (digit - 1) * 3; i < digit * 3; i++) {
+            char ch = (char) ('a' + i);
+            count = count + padCount(p + ch, up.substring(1));
+        }
+        return count;
+    }
+}
